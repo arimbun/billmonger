52c44b90934a2c040c19551dc671af91798bed9b
Arthur Rimbun <arthur@arimbun.net>
2019-12-03 22:44:20 +1100

    Change Invoice # to a number

--- invoice/bill.go
+++ invoice/bill.go
@@ -105,7 +105,7 @@ func (b *Bill) makeHeader() func() {
 		b.darkText()
 		b.text(20, 0, "Invoice #:")
 		b.lightText()
-		b.text(20, 0, billTime.EndOfMonth().Format("Jan22006"))
+		b.text(20, 0, "11") // TODO: Make this configurable
 
 		// Biller Name, Address
 		b.pdf.SetXY(8, 40)
