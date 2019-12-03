1643dae74769a55046201c4d321fc24fc5763392
Arthur Rimbun <arthur@arimbun.net>
2019-12-03 22:46:38 +1100

    Adjust cell widths

--- invoice/bill.go
+++ invoice/bill.go
@@ -148,7 +148,7 @@ func (b *Bill) RenderToFile() error {
 	b.drawBillTable(headers, b.config.Bill.Strings())
 
 	headers = []string{"Week", "Description", "Unit Price", "Line Total"}
-	widths := []float64{16, 125.5, 25, 25}
+	widths := []float64{49, 92.5, 25, 25}
 
 	b.drawBillablesTable(headers, b.config.Billables, b.config.Tax, widths)
 	b.drawBankDetails()
